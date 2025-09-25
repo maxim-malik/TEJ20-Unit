f172008a2c74aff571d3bae3836374a686461883
maxim-malik <maxim.malik@stu.ocsb.ca>
2025-09-25 16:42:29 -0400

    changed read me

--- main.ts
+++ main.ts
@@ -4,11 +4,11 @@
  * Created on: Sep 2025
  * This program shows temperature of microbit 
 */
-basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-// When button A is pressed the microbit temperature is shown
 input.onButtonPressed(Button.A, () => {
+    basic.clearScreen()
+    //comment
     let temp = input.temperature()
     basic.showNumber(temp)
 })
